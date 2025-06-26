c324d00eef3a0739f44b0cdf9863a9d0480eb16e
Harsh <bunnynick10@gmail.com>
2025-06-26 19:48:57 +0530

    feat: integrate React Query by adding ReactQueryProvider and updating dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import ReactQueryProvider from "@/utils/React-Query-Provider/ReactQueryProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ReactQueryProvider>
+          {children}
+        </ReactQueryProvider>
       </body>
     </html>
   );
